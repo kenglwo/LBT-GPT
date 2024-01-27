7672bf0c50ed29394a5a6717cf56bd91f1584968
Kento Shigyo <kentoglwo@gmail.com>
2024-01-27 14:50:22 +0900

    :recycle: add migration file to modify student_infos table

---
+++ backend/db/migrate/20240127054501_rename_student_info_to_rename_student_infos.rb
@@ -0,0 +1,5 @@
+class RenameStudentInfoToRenameStudentInfos < ActiveRecord::Migration[6.1]
+  def change
+    rename_table :student_info, :student_infos
+  end
+end
--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_01_25_153148) do
+ActiveRecord::Schema.define(version: 2024_01_27_054501) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,7 +49,7 @@ ActiveRecord::Schema.define(version: 2024_01_25_153148) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table "student_info", force: :cascade do |t|
+  create_table "student_infos", force: :cascade do |t|
     t.string "student_id"
     t.string "student_name"
     t.datetime "created_at", precision: 6, null: false
